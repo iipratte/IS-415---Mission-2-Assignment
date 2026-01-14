3eecf6247914ae94055f0eeed6c2595b3d0f5d24
iipratte <iipratte@byu.edu>
2026-01-14 14:58:54 -0700

    Split program into two classes

--- Program.cs
+++ Program.cs
@@ -1,35 +1,32 @@
 ﻿using System;
 
-Console.WriteLine("Welcome to the dice throwing simulator!\n");
-Console.WriteLine("How many dice rolls would you like to simulate?");
-int rollCount = int.Parse(Console.ReadLine());
-
-int[] rolls = new int[13];
-
-int roll1;
-int roll2;
-int rollTotal;
-
-Random rnd = new Random();
-
-for (int i = 0; i < rollCount; i++)
+namespace DiceSimulator
 {
-    roll1 = rnd.Next(1, 7);
-    roll2 = rnd.Next(1, 7);
-    rollTotal = roll1 + roll2;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the dice throwing simulator!\n");
+            Console.WriteLine("How many dice rolls would you like to simulate?");
+            int rollCount = int.Parse(Console.ReadLine());
 
-    rolls[rollTotal]++;
+            Roller r = new Roller();
+
+            int[] resultsArray = r.RollSimulator(rollCount);
+
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+            Console.WriteLine("Total number of rolls = " + rollCount);
+
+            for (int i = 2; i <= 12; i++)
+            {
+                double percentage = ((double)resultsArray[i] / rollCount) * 100;
+                int asterisksCount = (int)Math.Round(percentage);
+                Console.WriteLine(i + ": " + new string('*', asterisksCount));
+            }
+
+            Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!\n");
+        }
+    }
+
 }
-
-Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
-Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
-Console.WriteLine("Total number of rolls = " + rollCount);
-
-for (int i = 2; i <= 12; i++)
-{
-    double percentage = ((double)rolls[i] / rollCount) * 100;
-    int asterisksCount = (int)Math.Round(percentage);
-    Console.WriteLine(i + ": " + new string('*', asterisksCount));
-}
-
-Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!\n");
---
+++ Roller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DiceSimulator
+{
+    public class Roller
+    {
+        public int[] RollSimulator(int rollCount)
+        {
+            int[] rolls = new int[13];
+
+            int roll1;
+            int roll2;
+            int rollTotal;
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < rollCount; i++)
+            {
+                roll1 = rnd.Next(1, 7);
+                roll2 = rnd.Next(1, 7);
+                rollTotal = roll1 + roll2;
+
+                rolls[rollTotal]++;
+
+            }
+
+            return rolls;
+        }
+    }
+
+}
