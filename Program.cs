b9aab1467d0ff5453e9966f1a05c66b243122220
iipratte <iipratte@byu.edu>
2026-01-14 14:31:03 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to the dice throwing simulator!\n");
+Console.WriteLine("How many dice rolls would you like to simulate?");
+int rollCount = int.Parse(Console.ReadLine());
+
+int[] rolls = new int[13];
+
+int roll1;
+int roll2;
+int rollTotal;
+
+Random rnd = new Random();
+
+for (int i = 0; i < rollCount; i++)
+{
+    roll1 = rnd.Next(1, 7);
+    roll2 = rnd.Next(1, 7);
+    rollTotal = roll1 + roll2;
+
+    rolls[rollTotal]++;
+}
+
+Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+Console.WriteLine("Total number of rolls = " + rollCount);
+
+for (int i = 2; i <= 12; i++)
+{
+    double percentage = ((double)rolls[i] / rollCount) * 100;
+    int asterisksCount = (int)Math.Round(percentage);
+    Console.WriteLine(i + ": " + new string('*', asterisksCount));
+}
+
+Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!\n");
